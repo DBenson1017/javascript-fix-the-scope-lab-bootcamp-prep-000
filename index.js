f9e12da9b933cb536af9e4fe87c9f713b1563c3a
Devin Benson <devinbenson@gmail.com>
2020-01-26 20:46:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function add2(n) {
 function funkyFunction(){
 return function theFunk() {
 return 'FUNKY!'}
-theFunk()
+theFunk();
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
