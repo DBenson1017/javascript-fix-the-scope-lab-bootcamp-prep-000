271d4d933d6fbd1fd052840f2192a415d4a207f4
Devin Benson <devinbenson@gmail.com>
2020-01-26 21:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 
+
 function outer() { 
     function inner() {
         alert("hi");
